9935d16ea48dd63017241c5ff5ddc01e5ed7eb6a
Marcus Chui <marcus.chui.ca@icloud.com>
2023-03-18 21:32:09 -0400

    update deploy setting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  // base: "/COMP229-Project-Group3-Frontend/",
+  // base: "/",
+  base: "/COMP229-Project-Group3-Frontend/",
 });
